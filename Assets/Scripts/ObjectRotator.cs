6a9879354888194db9a9b0ea4b4dc32e41d0dfbb
parthlr <parthlr321@gmail.com>
2022-09-04 23:19:29 -0400

    Finished pool and solar system

--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectRotator : MonoBehaviour
 {
-    float orbitSpeed = .1f;
+    public float orbitSpeed = .1f;
 
     // Start is called before the first frame update
     void Start()
